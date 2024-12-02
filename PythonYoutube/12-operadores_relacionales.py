836121fa4285926a74006f8bd93d165fcdc83c0a
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-01 21:23:58 -0500

    adición operadores

--- PythonYoutube/12-operadores_relacionales.py
+++ PythonYoutube/12-operadores_relacionales.py
@@ -1,7 +1,7 @@
 """
 Programa que muestra el uso de operadores relacionales en Python
 Autor: Sharif Giraldo Obando
-Fecha: Noviembre de 2024
+Fecha: Diciembre de 2024
 Licencia: GNU GPL v3
 """
 #Devuelve True o False 
@@ -13,7 +13,29 @@ print(3<=4)
 print(3>=4)
 print(3==3)
 print(3!=4)
+print(3>4==2)
+
+"""
+Así como se pueden aplicar operadores aritméticos
+en las cadenas, también aplica el uso de operadores
+relacionales con datos numéricos y cadenas
+
+"""
 
 print("Hola">"Ornitorrinco")#Aquí procede con la longitud de las cadenas 
+print("Hola"<"Ornitorrinco")
+print("Hola"<="Ornitorrinco")
+print("Hola">="Ornitorrinco")
+print("Hola"=="Ornitorrinco")
+print("Hola"!="Ornitorrinco")
+print("Hola"=="Hola")
+print("Hola"=="Bola")#Ordenación alfabética por ASCII, resulta en false 
 
+print(len("Hola"))
+print(len("Ornitorrinco"))
 
+print(len("Hola")==len("Bola"))#Cuenta caracteres
+
+print("AA"=="aa")
+print("AA"=="aa".upper())
+print("AA".lower()=="aa")
---
+++ PythonYoutube/13-operadores_logicos.py
@@ -0,0 +1,35 @@
+"""
+Programa que muestra el uso de operadores lógicos en Python
+Autor: Sharif Giraldo Obando
+Fecha: Diciembre de 2024
+Licencia: GNU GPL v3
+"""
+
+print(not(True))
+print(not(False))
+print(True and False)
+print(True or False)
+
+print("\n")
+
+print(3>4 and "Hola">"Python")
+print(3>4 or "Hola">"Python" )
+
+print("\n")
+
+print(3<4 and "Hola">"Python")
+print(3<4 and "Hola"<"Python")
+print(3>4 or "Hola"<"Python")
+
+print("\n")
+
+print(3<4 or ("Hola">"Python" and 4==4))
+
+print("\n")
+
+print(not(3>4))
+print(not(3<4))
+
+
+
+
