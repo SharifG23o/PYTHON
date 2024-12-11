39b2b6c5bd2d8a24fbf7d90081918184c93f4ee3
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:28:19 -0500

    Operadores Lógicos

--- Python Básico/13-operadores_logicos.py
+++ Python Básico/13-operadores_logicos.py
@@ -5,31 +5,27 @@ Fecha: Diciembre de 2024
 Licencia: GNU GPL v3
 """
 
-print(not(True))
-print(not(False))
+print(not (True))
+print(not (False))
 print(True and False)
 print(True or False)
 
 print("\n")
 
-print(3>4 and "Hola">"Python")
-print(3>4 or "Hola">"Python" )
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
 
 print("\n")
 
-print(3<4 and "Hola">"Python")
-print(3<4 and "Hola"<"Python")
-print(3>4 or "Hola"<"Python")
+print(3 < 4 and "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 > 4 or "Hola" < "Python")
 
 print("\n")
 
-print(3<4 or ("Hola">"Python" and 4==4))
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
 
 print("\n")
 
-print(not(3>4))
-print(not(3<4))
-
-
-
-
+print(not (3 > 4))
+print(not (3 < 4))
