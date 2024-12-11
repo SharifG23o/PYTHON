064871b3ebb94a7328981293713a9babba6a7265
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:22:00 -0500

    Hola Mundo

--- Python Básico/00-hello.py
+++ Python Básico/00-hello.py
@@ -5,6 +5,7 @@ Fecha: Noviembre 2024
 Licencia: GNU GPL v3
 Esto es un comentario de varias líneas 
 """
+
 # Esto es un comentario de una sola línea
 # Primer Hola mundo en Python
 print("Hola mundo")  # Imprime la cadena de texto
