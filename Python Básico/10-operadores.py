884d4f12b044e9733b71dcd0198f89b5f05a98d4
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:26:51 -0500

    Operadores

--- Python Básico/10-operadores.py
+++ Python Básico/10-operadores.py
@@ -6,16 +6,17 @@ Licencia: GNU GPL v3
 """
 
 
+print("hola"+"python")  # concatena cadenas
+print("hola"*5)  # Imprime la cantidad de veces indicada
 
-print("hola"+"python")#concatena cadenas
-print("hola"*5)#Imprime la cantidad de veces indicada
+my_int = 2*2
+print("Hola"*my_int)  # imprime las veces de la multiplicación
 
-my_int=2*2
-print("Hola"*my_int)#imprime las veces de la multiplicación
-
-my_float=2.5*2
-print("Hola"*int(my_float))#Cambio el float por int y obtengo la cadena multiplicada
+my_float = 2.5*2
+# Cambio el float por int y obtengo la cadena multiplicada
+print("Hola"*int(my_float))
 
 
-my_float=2.5*2
-print("Hola"+str(my_float))#Concatena y dado el cambio de tipo se unen la cadena y el float ahora str
+my_float = 2.5*2
+# Concatena y dado el cambio de tipo se unen la cadena y el float ahora str
+print("Hola"+str(my_float))
