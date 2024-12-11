b88acd45d25d4cb2c9ec80ca7ba53cdae00aed06
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:23:16 -0500

    Convertir Datos

--- Python Básico/03-convertir_datos.py
+++ Python Básico/03-convertir_datos.py
@@ -5,9 +5,9 @@ Fecha: Noviembre de 2024
 Licencia: GNU GPL v3
 """
 
-edad_a_string=str(23)
-#Transforma en una cadena de texto
-edad_a_float=float(45)
-#Transforma en un float
+edad_a_string = str(23)
+# Transforma en una cadena de texto
+edad_a_float = float(45)
+# Transforma en un float
 print(type(edad_a_string))
 print(type(edad_a_float))
