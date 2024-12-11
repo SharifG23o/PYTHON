de54c28f709bcc15fb279309e51b75eba802a0fb
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:27:23 -0500

    Operadores Aritméticos

--- Python Básico/11-operadores_aritmeticos.py
+++ Python Básico/11-operadores_aritmeticos.py
@@ -6,13 +6,12 @@ Licencia: GNU GPL v3
 """
 
 
-#OPERADORES ARITMÉTICOS
-print(3+4)#Suma
-print(3*1)#Multiplicación
-print(3/4)#División
-print(3-4)#Resta
-print(3//4)#División exacta que devuelva un entero 
-print(3%4)#Módulo muestra el resto de la división
-print(2**3)#Elevar a la potencia 
-print(2**3+3-7/1//4)#Diferentes operaciones 
-
+# OPERADORES ARITMÉTICOS
+print(3+4)  # Suma
+print(3*1)  # Multiplicación
+print(3/4)  # División
+print(3-4)  # Resta
+print(3//4)  # División exacta que devuelva un entero
+print(3 % 4)  # Módulo muestra el resto de la división
+print(2**3)  # Elevar a la potencia
+print(2**3+3-7/1//4)  # Diferentes operaciones
