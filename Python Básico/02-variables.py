3eedd17b84ef2099a876d0ef2e53ca657ebd1bc5
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:22:48 -0500

    Variables

--- Python Básico/02-variables.py
+++ Python Básico/02-variables.py
@@ -5,23 +5,23 @@ Fecha: Noviembre de 2024
 Licencia: GNU GPL v3
 """
 
-#Python está en nomenclatura snake_case
-my_variable="Esto es una variable"
+# Python está en nomenclatura snake_case
+my_variable = "Esto es una variable"
 print(my_variable)
 
 
-edad=20
+edad = 20
 
 print(f"edad: {edad}")
 
-int_edad=28
+int_edad = 28
 print(int_edad)
 
-variable_bool=False
+variable_bool = False
 print(variable_bool)
 
 
 print(my_variable, edad, variable_bool)
-#imprime diferentes argumentos o datos
+# imprime diferentes argumentos o datos
 
-print(type(my_variable,variable_bool))
+print(type(my_variable, variable_bool))
