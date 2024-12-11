f2ff7e89630fedf08e751952a9263621a44c10d7
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:27:49 -0500

    Operadores Relacionales

--- Python Básico/12-operadores_relacionales.py
+++ Python Básico/12-operadores_relacionales.py
@@ -4,16 +4,16 @@ Autor: Sharif Giraldo Obando
 Fecha: Diciembre de 2024
 Licencia: GNU GPL v3
 """
-#Devuelve True o False 
-#Operadores relacionales o comparativosS
+# Devuelve True o False
+# Operadores relacionales o comparativosS
 
-print(3>4)
-print(3<4)
-print(3<=4)
-print(3>=4)
-print(3==3)
-print(3!=4)
-print(3>4==2)
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 3)
+print(3 != 4)
+print(3 > 4 == 2)
 
 """
 Así como se pueden aplicar operadores aritméticos
@@ -22,20 +22,20 @@ relacionales con datos numéricos y cadenas
 
 """
 
-print("Hola">"Ornitorrinco")#Aquí procede con la longitud de las cadenas 
-print("Hola"<"Ornitorrinco")
-print("Hola"<="Ornitorrinco")
-print("Hola">="Ornitorrinco")
-print("Hola"=="Ornitorrinco")
-print("Hola"!="Ornitorrinco")
-print("Hola"=="Hola")
-print("Hola"=="Bola")#Ordenación alfabética por ASCII, resulta en false 
+print("Hola" > "Ornitorrinco")  # Aquí procede con la longitud de las cadenas
+print("Hola" < "Ornitorrinco")
+print("Hola" <= "Ornitorrinco")
+print("Hola" >= "Ornitorrinco")
+print("Hola" == "Ornitorrinco")
+print("Hola" != "Ornitorrinco")
+print("Hola" == "Hola")
+print("Hola" == "Bola")  # Ordenación alfabética por ASCII, resulta en false
 
 print(len("Hola"))
 print(len("Ornitorrinco"))
 
-print(len("Hola")==len("Bola"))#Cuenta caracteres
+print(len("Hola") == len("Bola"))  # Cuenta caracteres
 
-print("AA"=="aa")
-print("AA"=="aa".upper())
-print("AA".lower()=="aa")
+print("AA" == "aa")
+print("AA" == "aa".upper())
+print("AA".lower() == "aa")
