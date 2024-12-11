28d851741f37322f907d5c3ea519e62bcf9448a3
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:29:38 -0500

    Funciones con Strings

--- Python Básico/16-funciones_con_strings.py
+++ Python Básico/16-funciones_con_strings.py
@@ -5,16 +5,16 @@ Fecha: Diciembre de 2024
 Licencia: GNU GPL v3
 """
 
-language="python"
-a,b,c,d,e,f=language
+language = "python"
+a, b, c, d, e, f = language
 
-print(language.capitalize())#Pone la primera letra en mayúscula
-print(language.upper())#Todo en mayúsculas
-print(language.lower())#Todo en minúsculas
-print(language.count("a"))#Cuenta cuántos caracteres hay
-print(len(language))#Imprime la longitud de la variable
-print(language.isnumeric())#Me dice si es un número
-print(language.isupper())#Me dice si está en minúscula
+print(language.capitalize())  # Pone la primera letra en mayúscula
+print(language.upper())  # Todo en mayúsculas
+print(language.lower())  # Todo en minúsculas
+print(language.count("a"))  # Cuenta cuántos caracteres hay
+print(len(language))  # Imprime la longitud de la variable
+print(language.isnumeric())  # Me dice si es un número
+print(language.isupper())  # Me dice si está en minúscula
 print(language.isalpha())
 print(language.isascii())
 print(language.islower())
