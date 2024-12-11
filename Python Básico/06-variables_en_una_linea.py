cfb51f63d8c909110430a1f20242e88829ff3a39
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-10 21:24:50 -0500

    Variables en una línea

--- Python Básico/06-variables_en_una_linea.py
+++ Python Básico/06-variables_en_una_linea.py
@@ -4,6 +4,6 @@ Autor: Sharif Giraldo Obando
 Fecha: Noviembre de 2024
 Licencia: GNU GPL v3
 """
-#Aquí se presentan variables en una sola línea
-name, surname,alias,age="Sharif", "Giraldo", "Shaggy",19
-print(age,surname,alias,"Mi nombre es:",name)
+# Aquí se presentan variables en una sola línea
+name, surname, alias, age = "Sharif", "Giraldo", "Shaggy", 19
+print(age, surname, alias, "Mi nombre es:", name)
