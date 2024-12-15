58202a16c335e77ba8e254cd08d86a28d2b1595c
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-14 19:02:17 -0500

    Módulo de ejemplo de funciones

---
+++ Python Básico/modulo.py
@@ -0,0 +1,13 @@
+"""
+Módulo de funciones
+Autor: Sharif Giraldo Obando
+Fecha: Diciembre de 2024
+Licencia: GNU GPL v3
+"""
+
+def sumar_dos_valores(numero_uno,numero_dos):
+    print(numero_uno+numero_dos)
+
+
+def imprimir_nombre(nombre, apellido):
+    print(f"{nombre} {apellido}")
